1d9ff0a030395a0dfa0d06639d0bb3b82255d094
rushi-smb <rushi@smbios.co.in>
2023-10-16 19:16:08 +0530

    Add reference to the pipeline and add namespace to the xaml file of skiasharp

--- DaluiApp/MauiProgram.cs
+++ DaluiApp/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 using PanCardView;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace DaluiApp
 {
@@ -12,6 +13,7 @@ namespace DaluiApp
             builder
                 .UseMauiApp<App>()
                 .UseCardsView()
+                .UseSkiaSharp()
                 .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
