58ffa08a6b94bc6490bd41cc1dfb3a379104f655
rushi-smb <rushi@smbios.co.in>
2023-10-16 22:11:41 +0530

    Generate AI Image using StopWatch

--- DaluiApp/Views/ImageGeneratorView.xaml.cs
+++ DaluiApp/Views/ImageGeneratorView.xaml.cs
@@ -1,21 +1,63 @@
+using System.Diagnostics;
+
 namespace DaluiApp.Views;
 
 public partial class ImageGeneratorView : ContentPage
 {
+    private Stopwatch watch = new Stopwatch();
     public ImageGeneratorView()
     {
         InitializeComponent();
     }
 
-    private void BtnFinish_OnClicked(object sender, EventArgs e)
+    protected override async void OnAppearing()
     {
-        StopGeneration();
+        await Task.Delay(TimeSpan.FromSeconds(2));
+        watch.Start();
+
+        var cts = new CancellationTokenSource();
+
+        using (var timer = new PeriodicTimer(TimeSpan.FromSeconds(1)))
+        {
+            try
+            {
+                var counter = 0;
+                while (await timer.WaitForNextTickAsync(cts.Token))
+                {
+                    if (counter == 5)
+                    {
+                        cts.Cancel();
+                    }
+
+                    var seconds = watch.Elapsed.Seconds;
+                    lblTimer.Text = seconds.ToString();
+                    counter++;
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                await StopGeneration();
+            }
+        }
     }
 
-    private void StopGeneration()
+    private async Task StopGeneration()
     {
+        watch.Stop();
+        await Task.Delay(2000);
+
         lottie.IsAnimationEnabled = false;
         lottie.IsVisible = false;
         imageBorder.IsVisible = true;
+        imageAnimation.IsVisible = true;
+
+        await Task.WhenAny(
+            imageAnimation.ScaleTo(1.1, 1000),
+            imageAnimation.FadeTo(0, 1000),
+            borderTime.ScaleTo(1, 1000),
+            borderTime.FadeTo(1, 1000));
+
+        await borderTime.FadeTo(0, 300);
+        await btnFinish.ScaleTo(1, 1000);
     }
 }
